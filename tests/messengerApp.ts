0a0f887dffb1b8b6d5b059ee700c3c10c3091f3c
Mayank Mittal <mmittal123456@gmail.com>
2022-01-19 17:21:04 +0530

    upload

---
+++ tests/messengerApp.ts
@@ -0,0 +1,40 @@
+import * as anchor from '@project-serum/anchor';
+import * as assert from 'assert';
+const { SystemProgram } = anchor.web3;
+let _baseAccount: anchor.web3.Keypair;
+describe("Testing our messaging app: ", () => {
+  const provider = anchor.Provider.env()
+  anchor.setProvider(provider);
+  const program = anchor.workspace.MessengerApp;
+
+  it("An account is initialized", async function () {
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.initialize("My first message", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount]
+    });
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Data', account.data);
+    assert.ok(account.data === "My first message")
+    _baseAccount = baseAccount;
+  });
+
+  it("Update the account previously created: ", async function () {
+    const baseAccount = _baseAccount;
+    await program.rpc.update("My second message", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      }
+    });
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log("Updated data: ", account.data);
+    assert.ok(account.data === "My second message");
+    console.log("All account data: ", account)
+    console.log("All data: ", account.dataList);
+    assert.ok(account.dataList.length === 2)
+  })
+})
